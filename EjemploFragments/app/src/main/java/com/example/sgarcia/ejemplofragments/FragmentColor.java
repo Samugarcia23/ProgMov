9cecd108767fd8e9720be9bf0027aed3fa10ee8c
Sam <samugarcia23@gmail.com>
2018-12-18 10:29:08 +0100

    mini arreglo fragments

--- EjemploFragments/app/src/main/java/com/example/sgarcia/ejemplofragments/FragmentColor.java
+++ EjemploFragments/app/src/main/java/com/example/sgarcia/ejemplofragments/FragmentColor.java
@@ -4,29 +4,32 @@ import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
-import android.app.Fragment;
+
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class FragmentColor extends Fragment implements LifecycleOwner {
+public class FragmentColor extends Fragment {
+
 
     Button btnRed, btnBlue;
     public ViewModel miViewmodel;
     String color;
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.color_layout, container, false);
-
+        miViewmodel = ViewModelProviders.of(this).get(MainViewModel.class);
         final Observer<String> observer = new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -34,14 +37,9 @@ public class FragmentColor extends Fragment implements LifecycleOwner {
             }
         };
 
+
         ((MainViewModel) miViewmodel).getColor().observe( this, observer);
 
         return rootView;
-    }
-
-    @NonNull
-    @Override
-    public Lifecycle getLifecycle() {
-        return ;
     }
 }
--- EjemploFragments/app/src/main/java/com/example/sgarcia/ejemplofragments/MainActivity.java
+++ EjemploFragments/app/src/main/java/com/example/sgarcia/ejemplofragments/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.sgarcia.ejemplofragments;
 
 import android.arch.lifecycle.ViewModel;
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
@@ -18,6 +14,7 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     Button btnRed, btnBlue;
     public ViewModel miViewmodel;
     String color;
+    FragmentColor fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +23,10 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
         btnRed = findViewById(R.id.btnRed);
         btnBlue = findViewById(R.id.btnBlue);
+        fragment = new FragmentColor();
         miViewmodel = ViewModelProviders.of(this).get(MainViewModel.class);
+
+        replaceFragment(fragment);
 
         btnRed.setOnClickListener(this);
         btnBlue.setOnClickListener(this);
@@ -35,27 +35,23 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        FragmentColor fragment;
+
         switch (v.getId()){
 
             case R.id.btnRed:
-                fragment = new FragmentColor();
                 color = "#f74a4a";
                 ((MainViewModel) miViewmodel).setColor(color);
-                replaceFragment(fragment);
                 break;
 
             case R.id.btnBlue:
-                fragment = new FragmentColor();
                 color = "#4a7df7";
                 ((MainViewModel) miViewmodel).setColor(color);
-                replaceFragment(fragment);
                 break;
         }
     }
 
     public void replaceFragment(FragmentColor someFragment) {
-        android.app.FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.your_placeholder, someFragment);
         transaction.addToBackStack(null);
         transaction.commit();
